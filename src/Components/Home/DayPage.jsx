b432a2cb050ad791598b8fd63a8ee64773e58308
Abdullah Subayeel <abdullahsubayeel@gmail.com>
2022-11-11 09:59:21 +0530

    ss

--- src/Components/Home/DayPage.jsx
+++ src/Components/Home/DayPage.jsx
@@ -91,7 +91,7 @@ const DayPage = () => {
       </MDBRow>
       <MDBCard className="m-2">
         <MDBCardHeader className=" fs-3 text fw-bolder">
-          Day-{day[3]} Match paused due to rain
+          Day-{day[3]}
         </MDBCardHeader>
         {dayObj.liveLink === "" ? (
           ""
